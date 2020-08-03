2a2657789dd7966099c3bb4426b0180b1d5893e2
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-08-03 12:46:25 -0700

    fixes bug

--- features/monty_python.js
+++ features/monty_python.js
@@ -8,49 +8,65 @@ module.exports = function(controller) {
   controller.hears('hello', 'message', async(bot, message) => {
        await bot.beginDialog(MY_DIALOG_ID);
   });
-  //
+
   convo.say('Stop! Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.');
 
-// ask a question, store the response in 'name' 
-convo.ask('What is your name?', async(response, convo, bot) => {
-    console.log(`user name is ${ response }`);
-    // do something?
-}, 'name');
+  convo.ask('What is your name?', async(response, convo, bot) => {
+      console.log(`user name is ${ response }`);
+  }, 'name');
 
-convo.addAction('the_quest');
 
-convo.ask('what...is your quest?', async(response, convo, bot) => {
-  console.log(`users quest is ${ response }`);
-}, 'quest', 'the_quest');
+  convo.ask('what...is your quest?', async(response, convo, bot) => {
+    console.log(`users quest is ${ response }`);
+  }, 'quest', 'the_quest');
 
-// use add action to switch to a different thread, defined below...
-convo.addAction('favorite_color');
+  convo.ask('what...is your favorite color?', async(response, convo, bot) => {
+      console.log(`user favorite color is ${ response }`);
+      if (response != "blue"){
+      bot.reply('AUGH!')} else {
+        bot.reply('Right then, off you go.')
+      };
+  },'color', 'favorite_color');
 
-// add a message and a prompt to a new thread called `favorite_color`
-convo.addMessage('what...', 'favorite_color');
-convo.addQuestion('...is your favorite color?', async(response, convo, bot) => {
-    console.log(`user favorite color is ${ response }`);
-},'color', 'favorite_color');
+  // convo.say('AUGH!!');
 
+  controller.afterDialog(convo, async(bot, results) => {
+
+      // use results of dialog here
+      let name = results.name;
+      let color = results.color;
+
+      if (name=="Sir Lancelot" && color=="blue") {
+        convo.say("Right, off you go!") } else {
+          convo.say("AUGH!")
+        };
+
+      // do some cool database stuff here
+
+      // start next dialog
+      // await bot.beginDialog(NEXT_DIALOG);
+
+  });
 // go to a confirmation
-convo.addAction('confirmation' ,'favorite_color');
-
-// do a simple conditional branch looking for user to say "no"
-convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.color}}. Is that right?', [
-    {
-        pattern: 'no',
-        handler: async(response, convo, bot) => {
-            // if user says no, go back to favorite color.
-            await convo.gotoThread('favorite_color');
-        }
-    },
-    {
-        default: true,
-        handler: async(response, convo, bot) => {
-            // do nothing, allow convo to complete.
-        }
-    }
-], 'confirm', 'confirmation');
+// convo.addAction('confirmation' ,'favorite_color');
+//
+//
+// // do a simple conditional branch looking for user to say "no"
+// convo.addQuestion('Your name is {{vars.name}} and your favorite color is {{vars.color}}. Is that right?', [
+//     {
+//         pattern: 'no',
+//         handler: async(response, convo, bot) => {
+//             // if user says no, go back to favorite color.
+//             await convo.gotoThread('favorite_color');
+//         }
+//     },
+//     {
+//         default: true,
+//         handler: async(response, convo, bot) => {
+//             // do nothing, allow convo to complete.
+//         }
+//     }
+// ], 'confirm', 'confirmation');
 
   controller.addDialog(convo);
 };
