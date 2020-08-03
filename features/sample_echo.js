4dac230690a9805b08c1880d07b2e9bd5d31ee1a
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-08-03 10:30:37 -0700

    adds lancelot thread

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -9,6 +9,22 @@ module.exports = function(controller) {
         await bot.reply(message, 'I heard a sample message.');
     });
 
+    controller.hears('ask me the questions, bridgekeeper, I am not afraid', 'message,direct_message', async(bot, message) => {
+      await bot.reply(message, 'What...is your name?');
+    });
+
+    controller.hears('Sir Lancelot of Camelot', 'message,direct_message', async(bot, message) => {
+      await bot.reply(message, 'What...is your quest?');
+    });
+
+    controller.hears('To seek the holy grail', 'message,direct_message', async(bot, message) => {
+      await bot.reply(message, 'What... is your favorite color?');
+    });
+
+    controller.hears('Blue', 'message,direct_message', async(bot, message) => {
+      await bot.reply(message, 'Right. Off you go.');
+    });
+
     controller.on('message,direct_message', async(bot, message) => {
         await bot.reply(message, `Echo: ${ message.text }`);
     });
